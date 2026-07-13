f64d5e41ab9268abe2036b1190b9a37eb2b06220
PureCodeee <ivan1998xxxxx@gmail.com>
2026-07-13 18:03:54 +0300

    feat: Enhance user API by adding UsersResponse model and updating get_users endpoint

--- app/models.py
+++ app/models.py
@@ -10,6 +10,10 @@ class CreateUser(BaseModel):
     name: str
 
 
+class UsersResponse(BaseModel):
+    users: list[User]
+
+
 users: list[User] = [
     User(id=1, name="genesis_user")
 ]
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, status
-from app.models import users, CreateUser, User
+from app.models import UsersResponse, users, CreateUser, User
 
 
 router = APIRouter(prefix="/api/users")
 
 
-@router.get("/", response_model=list[User])
+@router.get("/", response_model=UsersResponse)
 async def get_users():
-    return users
+    return {"users": users}
 
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def create_user(user: CreateUser):
-    new_id = max((user.id for user in users), default=0) + 1
+    new_id = max((u.id for u in users), default=0) + 1
     new_user = User(id=new_id, name=user.name)
     users.append(new_user)
     return new_user
