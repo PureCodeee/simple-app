e2d5432294b75b968a1f4c20d983c3918803daf8
PureCodeee <ivan1998xxxxx@gmail.com>
2026-07-13 19:42:41 +0300

    test: добавить тесты для REST API пользователей

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -29,3 +29,76 @@ def test_health_check():
     assert response.json() == {
         "status": "ok"
     }
+
+
+def test_get_users():
+    response = client.get("/api/users/")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["users"][0]["id"] == 1
+    assert data["users"][0]["name"] == "genesis_user"
+
+
+def test_create_user():
+    response = client.post("/api/users/", json={"name": "new_user"})
+
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["id"] == 2
+    assert data["name"] == "new_user"
+    assert len(users) == 2
+    assert users[-1].id == 2
+    assert users[-1].name == "new_user"
+
+
+def test_create_user_validation():
+    response = client.post(
+        "/api/users/",
+        json={}
+    )
+
+    assert response.status_code == 422
+
+    data = response.json()
+
+    assert "detail" in data
+
+
+def test_get_user():
+    response = client.get("/api/users/1")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["id"] == 1
+    assert data["name"] == "genesis_user"
+
+
+def test_get_user_not_found():
+    response = client.get("/api/users/9999")
+
+    assert response.status_code == 404
+
+
+def test_delete_user():
+    response = client.delete("/api/users/1")
+
+    assert response.status_code == 204
+    assert len(users) == 0
+
+    response = client.get("/api/users/1")
+
+    assert response.status_code == 404
+
+
+
+def test_delete_user_not_found():
+    response = client.delete("/api/users/999")
+
+    assert response.status_code == 404
